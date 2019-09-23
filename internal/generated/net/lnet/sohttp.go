d63447cef629a596d2fc424521ecd5c4d44e7d67
zhangjin <zhangjin@cmcm.com>
2019-09-24 01:07:06 +0800

    perf:新增post的测试方法

--- internal/generated/net/lnet/sohttp.go
+++ internal/generated/net/lnet/sohttp.go
@@ -8,8 +8,8 @@ import (
 )
 
 // handler 定义
-var (
-	CommonTimeSoGinHandler = &handler.Handler{
+var hs = []*handler.Handler{
+	&handler.Handler{
 		Name:    "common.time",
 		Routers: sohttp.NewRouters(handlers.CommonTimeURIS, handlers.CommonTimeHTTPMethods),
 		Req:     &handlers.CommonTimeReq{},
@@ -17,8 +17,17 @@ var (
 		Func: func(ctx context.Context, req, resp interface{}) error {
 			return handlers.CommonTimeHandle(ctx, req.(*handlers.CommonTimeReq), resp.(*handlers.CommonTimeResp))
 		},
-	}
-)
+	},
+	&handler.Handler{
+		Name:    "common.test",
+		Routers: sohttp.NewRouters(handlers.CommonPostURIS, handlers.CommonPostHTTPMethods),
+		Req:     &handlers.CommonPostReq{},
+		Resp:    &handlers.CommonPostResp{},
+		Func: func(ctx context.Context, req, resp interface{}) error {
+			return handlers.CommonPostHandle(ctx, req.(*handlers.CommonPostReq), resp.(*handlers.CommonPostResp))
+		},
+	},
+}
 
 // SoHTTP 获取 gnet http 服务
 func SoHTTP() (*sohttp.SoHTTP, error) {
@@ -26,6 +35,8 @@ func SoHTTP() (*sohttp.SoHTTP, error) {
 	if err != nil {
 		return nil, err
 	}
-	s.Register(CommonTimeSoGinHandler)
+	for _, h := range hs {
+		s.Register(h)
+	}
 	return s, nil
 }
