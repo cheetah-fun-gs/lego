8514024278edee672bdc31ae20e465a490927bd6
zhangjin <zhangjin@cmcm.com>
2019-12-24 14:44:51 +0800

    perf:添加handle之前之后的注入函数

--- pkg/core/handler.go
+++ pkg/core/handler.go
@@ -6,18 +6,22 @@ import (
 
 // Handler 处理器定义
 type Handler interface {
-	GetName() string               // 名称
-	GetRouter() (routers []Router) // 路由器对象 允许多个路由器指向相同处理器 router 如果是结构体必须提供 String() 方法
-	CloneReq() interface{}         // 克隆一个请求结构体的新指针
-	CloneResp() interface{}        // 克隆一个响应结构体的新指针
-	Handle(ctx context.Context, req, resp interface{}) error
+	GetName() string                                                    // 名称
+	GetRouter() (routers []Router)                                      // 路由器对象 允许多个路由器指向相同处理器
+	CloneReq() interface{}                                              // 克隆一个请求结构体的新指针
+	CloneResp() interface{}                                             // 克隆一个响应结构体的新指针
+	Handle(ctx context.Context, req, resp interface{}) error            // 处理方法
+	InjectBefore(f ...func(ctx context.Context, req interface{}))       // 在 handle 之前注入 异步并行
+	InjectBehind(f ...func(ctx context.Context, req, resp interface{})) // 在 handle 之后注入 异步并行
 }
 
 // DefaultHandler 默认处理器
 type DefaultHandler struct {
-	Name    string
-	Routers []Router // 路由器
-	Func    func(ctx context.Context, req, resp interface{}) error
+	Name       string
+	Routers    []Router // 路由器
+	Func       func(ctx context.Context, req, resp interface{}) error
+	beforeFunc []func(ctx context.Context, req interface{})
+	behindFunc []func(ctx context.Context, req, resp interface{})
 }
 
 // GetName 获取处理器名称
@@ -42,5 +46,24 @@ func (h *DefaultHandler) CloneResp() interface{} {
 
 // Handle 处理器方法
 func (h *DefaultHandler) Handle(ctx context.Context, req, resp interface{}) error {
-	return h.Func(ctx, req, resp)
+	for _, f := range h.beforeFunc {
+		go f(ctx, req)
+	}
+	if err := h.Func(ctx, req, resp); err != nil {
+		return err
+	}
+	for _, f := range h.behindFunc {
+		go f(ctx, req, resp)
+	}
+	return nil
+}
+
+// InjectBefore 在handle之前注入
+func (h *DefaultHandler) InjectBefore(f ...func(ctx context.Context, req interface{})) {
+	h.beforeFunc = append(h.beforeFunc, f...)
+}
+
+// InjectBehind 在handle之后注入
+func (h *DefaultHandler) InjectBehind(f ...func(ctx context.Context, req, resp interface{})) {
+	h.behindFunc = append(h.behindFunc, f...)
 }
